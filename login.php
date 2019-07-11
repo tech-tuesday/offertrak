d6dc4bf22c6f059ac1f7a50ecf32919d21a010a7
Kameron Welch <Kameronwelch18@gmail.com>
2019-07-10 21:09:17 -0400

    Lowercase Login

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<head><link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+<form action = "index.php" method = "post">
+
+	<div class="form-group">
+	<label>Email Address</label>
+	<input class = "form-control" name = "email_id" placeholder = "Enter Email Address" >
+	</div>
+
+	<div class="form-group">
+	<label >Password</label>
+	<input class = "form-control" name = "loginz_pw" placeholder = "Enter Password">
+	</div>
+	
+	<button class = "btn btn-primary" type = "sumbit" >Sumbit</button>
+	
+</form>
+
+
+<?php
+
+?>
