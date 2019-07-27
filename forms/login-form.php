405b2af53314d9d157dc3e0df3dcecaed8f0dd25
wilfred66 <wilfredpowelljr@gmail.com>
2019-07-27 12:16:06 -0400

    Fix Login

--- forms/login-form.php
+++ forms/login-form.php
@@ -1,6 +1,6 @@
 <<<<<<< HEAD
 <?php
-	include_once $_SERVER['DOCUMENT_ROOT'] . '/offertrak/tpl/app_header.php';
+	include_once $_SERVER['DOCUMENT_ROOT'] . '/offertrak/web-assets/tpl/app_header.php';
 ?>
 
 <div class="row mt-5">
@@ -20,12 +20,12 @@
 							<label >Password</label>
 							<input class = "form-control" name = "loginz_pw" placeholder = "Enter Password">
 							</div>
-							
+
 							<button class = "btn btn-primary" type = "submit" >Submit</button>
 
 						</div>
 					</div>
-					
+
 				</form>
 		</div>
 		<div class="col-2"></div>
