282263729c98271899745f6f2148c9fa356504cf
Eddie Gichuhi <gichedw@gmail.com>
2019-07-27 15:14:56 -0400

    updating bootstrap, header, footer

--- web-assets/tpl/app_footer.php
+++ web-assets/tpl/app_footer.php
@@ -1,8 +1,7 @@
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-</div>
-<script src="/bdpa-loans/web-assets/js/jquery-3.3.1.min.js"></script>
-<script src="/bdpa-loans/web-assets/js/popper.min.js"></script>
-<script src="/bdpa-loans/web-assets/js/bootstrap.min.js"></script>
+
+</main>
+<!-- jQuery first, then Bootstrap JS -->
+<script src="/offertrak/web-assets/js/jquery-3.4.1.min.js"></script>
+<script src="/offertrak/web-assets/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- web-assets/tpl/app_header.php
+++ web-assets/tpl/app_header.php
@@ -1,20 +1,24 @@
-<?php
-  session_start();
-  include_once $_SERVER['DOCUMENT_ROOT'] . '/offertrak/web-assets/lib/db.php';
-?>
-
-<!DOCTYPE html>
+<?php session_start(); ?>
+<!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+<!-- Bootstrap CSS -->
+<link href="/offertrak/web-assets/css/bootstrap.min.css?v=4.3.1" rel="stylesheet"/>
+<link href="/offertrak/web-assets/css/site.css" rel="stylesheet"/>
+<title><?php if ( isset($page_title) and !empty($page_title) ) { echo $page_title; } else { echo "OfferTrak"; } ?></title>
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <a class="navbar-brand" href="/offertrak/">OfferTrak</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-    <!-- Bootstrap CSS -->
+</nav>
 
-    <link rel="stylesheet" href="/offertrak/web-assets/css/bootstrap.min.css?>"> <!--I don't know why that href works necessarily. -->
+<main role="main" class="container-fluid">
 
-    <title><?php echo $page_title ?></title>
-  </head>
-  <body>
-    
+
