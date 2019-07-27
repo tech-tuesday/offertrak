8370842a8c1ba84bfe26c4619ed5fa95a34c6941
Christopher B <cballenger@steamsport.com>
2019-07-27 13:41:21 -0400

    The CSS works.

--- forms/login-form.php
+++ forms/login-form.php
@@ -1,41 +1,7 @@
-<<<<<<< HEAD
-<?php
-	include_once $_SERVER['DOCUMENT_ROOT'] . '/offertrak/web-assets/tpl/app_header.php';
-?>
-
-<div class="row mt-5">
-		<div class="col-2"></div>
-		<div class="col-8">
-				<form action = "index.php" method = "post">
-
-					<h3>Login</h3>
-					<div class="card">
-						<div class="card-body">
-							<div class="form-group">
-							<label>Email Address</label>
-							<input class = "form-control" name = "email_id" placeholder = "Enter Email Address" >
-							</div>
-
-							<div class="form-group">
-							<label >Password</label>
-							<input class = "form-control" name = "loginz_pw" placeholder = "Enter Password">
-							</div>
-
-							<button class = "btn btn-primary" type = "submit" >Submit</button>
-
-						</div>
-					</div>
-
-				</form>
-		</div>
-		<div class="col-2"></div>
-</div>
-
-
 <?php
 	include_once $_SERVER['DOCUMENT_ROOT'] . '/offertrak/web-assets/tpl/app_footer.php';
 ?>
-=======
+
 <form id="login" method="post" action="/offertrak/" autocomplete="false" class="needs-validation" novalidate>
   <div class="jumbotron">
   <div class="card">
@@ -72,5 +38,3 @@
   </div>
   </div>
 </form><br/>
-
->>>>>>> 6ea12d5cec4671a82e20feb929e79c1b92961f50
--- web-assets/tpl/app_header.php
+++ web-assets/tpl/app_header.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="/bdpa-loans/web-assets/css/bootstrap.min.css?>"> <!--I don't know why that href works necessarily. -->
+    <link rel="stylesheet" href="/offertrak/web-assets/css/bootstrap.min.css?>"> <!--I don't know why that href works necessarily. -->
 
     <title><?php echo $page_title ?></title>
   </head>
