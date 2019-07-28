b4f42ae9b46df07893b8ea21411734e0c502330c
Eddie Gichuhi <gichedw@gmail.com>
2019-07-27 23:56:57 -0400

    added client-side form validation javascript

--- forms/pw-reset-form.php
+++ forms/pw-reset-form.php
@@ -5,7 +5,7 @@
       <div class="alert alert-primary">Enter your email below to request a password reset</div>
       <div class="form-group">
         <div class= "input-group mx-auto mb-3 col-md-9">
-          <div class="input-group-prepend"><span class="input-group-text"><i class="fas fa-envelope"></i></span></div>
+          <div class="input-group-prepend"><span class="input-group-text">Email</div>
           <input type="email" class="form-control" id="email_id" name="email_id" value="<?php echo $email_id; ?>" required>
           <div class="invalid-feedback">Valid Email ID is required</div>
         </div>
@@ -14,8 +14,8 @@
       <div class="form-group">
         <div class= "col-md-9 mx-auto">
           <input id="w" type="hidden" name="w" value="pw_reset"/>
-          <button type="submit" class="btn btn-primary">Reset Password</button>
-          <a class="btn btn-outline-primary float-right" href="/offertrak/?w=login_f"><i class="fas fa-lock"></i> Login</a>
+          <button type="submit" class="btn btn-sm btn-primary">Reset Password</button>
+          <a class="btn btn-sm btn-outline-primary float-right" href="/offertrak/?w=login_f">Login</a>
         </div>
       </div>
 
--- web-assets/tpl/app_footer.php
+++ web-assets/tpl/app_footer.php
@@ -3,5 +3,29 @@
 <!-- jQuery first, then Bootstrap JS -->
 <script src="/offertrak/web-assets/js/jquery-3.4.1.min.js"></script>
 <script src="/offertrak/web-assets/js/bootstrap.bundle.min.js"></script>
+<script>
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+$( document ).ready(function() {
+  $(".clickable-row").click(function() {
+    window.location = $(this).data("href");
+  });
+});
+</script>
 </body>
 </html>
